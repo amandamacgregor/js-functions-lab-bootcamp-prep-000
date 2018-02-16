55aa5852cfb9348b474d2c1656f4b170839abf10
Amanda MacGregor <amandaseagull3@gmail.com>
2018-02-16 15:54:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,6 @@ function happyHolidaysTo(name) {
   return (`Happy holidays, ${name}!`)
 }
 happyHolidaysTo('you')
+function happyHolidayTo(holiday, name) {
+  return (`Happy ${holiday}, ${name}!`)
+}
